07abe5ec770481f20b9af4692c99321d6e520df8
rajendra <rajendra@appscrip.co>
2024-10-15 15:43:12 +0530

    added right most tab section

--- Daily-Tasks/Gmail-UI-clone/components/MainSection.js
+++ Daily-Tasks/Gmail-UI-clone/components/MainSection.js
@@ -1,3 +1,4 @@
+import EmailContentSection from "../ui/EmailContentSection.js";
 import PopSideBar from "../ui/PopSideBar.js";
 import SideBar from "../ui/SideBar.js";
 import applyStyle from "../utilities/applyStyle.js";
@@ -27,6 +28,57 @@ const gmailSpecificIconData = [
         iconText: 'Meet',
     },
 ]
+const googleAppIconData = [
+    {
+        iconName: 'mailIcon',
+        iconSrc: './assests/main-section/calendar.png',
+        iconWidth: 20,
+        iconHeight: 20,
+        iconText: '',
+    },
+    {
+        iconName: 'chatIcon',
+        iconSrc: './assests/main-section/keep.png',
+        iconWidth: 20,
+        iconHeight: 20,
+        iconText: '',
+    },
+    {
+        iconName: 'videoCallIcon',
+        iconSrc: './assests/main-section/tasks.png',
+        iconWidth: 20,
+        iconHeight: 20,
+        iconText: '',
+    },
+    {
+        iconName: 'videoCallIcon',
+        iconSrc: './assests/main-section/contacts.png',
+        iconWidth: 20,
+        iconHeight: 20,
+        iconText: '',
+    },
+    {
+        iconName: 'videoCallIcon',
+        iconSrc: './assests/main-section/minus-sign.png',
+        iconWidth: 20,
+        iconHeight: 20,
+        iconText: '',
+        opacity:'0.1',
+    },
+    {
+        iconName: 'videoCallIcon',
+        iconSrc: './assests/main-section/plus.png',
+        iconWidth: 24,
+        iconHeight: 24,
+        iconText: '',
+    },
+]
+
+const googleAppIconDataStyle = {
+    backgroundColor:'#f6f8fc',
+    paddingIconBox: '26px 26px',
+    isSymmetricEffect: true,
+}
 
 export default function MainSection(){
     const mainSectionDiv = createElement('div');
@@ -34,10 +86,11 @@ export default function MainSection(){
         width:'100%',
         height:'calc(100% - 55px)',
         display:'flex',
+        backgroundColor:'#f6f8fc',
     })
 
 
-    mainSectionDiv.append(SideBar(gmailSpecificIconData), PopSideBar())
+    mainSectionDiv.append(SideBar(gmailSpecificIconData), PopSideBar(), EmailContentSection(), SideBar(googleAppIconData, googleAppIconDataStyle))
 
 
     
