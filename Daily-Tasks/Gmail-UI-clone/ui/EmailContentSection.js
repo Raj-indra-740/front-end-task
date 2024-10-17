751067b204ece4215809ed0ea1c3eb9c7c18b6af
rajendra <rajendra@appscrip.co>
2024-10-17 17:18:39 +0530

    main email content section

--- Daily-Tasks/Gmail-UI-clone/ui/EmailContentSection.js
+++ Daily-Tasks/Gmail-UI-clone/ui/EmailContentSection.js
@@ -1,6 +1,8 @@
 import applyStyle from "../utilities/applyStyle.js";
 import addHoverEffect from "../utilities/addHoverEffect.js";
 import createImgContainer from "../utilities/createImgContainer.js";
+import EmailContentSectionHeader from "./EmailContentSectionHeader.js";
+import { emailData } from "../constants/emailData.js";
 
 const createElement = document.createElement.bind(document)
 
@@ -14,157 +16,262 @@ export default function EmailContentSection(){
         borderRadius:'16px',
     })
 
-    const emailContentSectionHead = createElement('div');
-    emailContentSectionHead.id = 'emailContentSectionHead'
-    applyStyle(emailContentSectionHead, {
-        display:'flex',
-        justifyContent:'flex-start',
-        alignItems:'center',
-        padding: '10px 16px'
+
+    const emailContentDiv = createElement('div');
+    emailContentDiv.id = 'emailContentDiv';
+
+    applyStyle(emailContentDiv,{
+        width:'100%',
+        height: 'calc(100% - 56px)',
+        overflowY:'scroll',
     })
 
-    //check box list section
-    const cheackBoxListDiv = createElement('div');
-    cheackBoxListDiv.id = 'cheackBoxListDiv'
-    applyStyle(cheackBoxListDiv, {
-        display:'flex',
-        alignItems:'center',
-    })
+    const emailContenSubDiv = createElement('div');
+    emailContenSubDiv.id = 'emailContenSubDiv';
 
-    const cheackBoxDiv = createElement('div');
-    applyStyle(cheackBoxDiv, {
-        padding:'5px 1px',
-        borderRadius:'5px',
-    })
-    const cheackBox = createElement('div')
-    applyStyle(cheackBox, {
-        width:'16px',
-        height:'16px',
-        margin:'5px',
-        border:'3px solid #5d5f5e',
-        borderRadius:'2px',
-        opacity:'0.7',
-    })
-    addHoverEffect(cheackBoxDiv, [{
-        styleProp:'backgroundColor',
-        styleValue: '#eaebef',
-    }])
+    emailData.forEach((item, i) => {
+        const emailContentRow = createElement('div');
+        emailContentRow.id = 'emailContentRow'+ item.id
+        applyStyle(emailContentRow, {
+            width:"100%",
+            backgroundColor:'#f2f6fc',
+            marginBottom:'0px',
+            padding:'5px 0',
+            display:'flex',
+            alignItems:'center',
+            // justifyContent:'space-between',
 
-    cheackBoxDiv.append(cheackBox)
+        })
 
-    //checkboc list drop down
-    const cheackBoxListDropDownIcon = createElement('div')
-    cheackBoxListDropDownIcon.id = 'cheackBoxListDropDownIcon';
-    applyStyle(cheackBoxListDropDownIcon, {
-        padding:'5px',
-        borderRadius:'5px',
+
+        const cheackBoxDiv = createElement('div');
+        applyStyle(cheackBoxDiv, {
+            width: '30px',
+            height: '30px',
+            borderRadius: '50%',
+            cursor: 'pointer',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginLeft:'15px '
+        })
+        const cheackBox = createElement('div')
+        applyStyle(cheackBox, {
+            width:'16px',
+            height:'16px',
+            margin:'5px',
+            border:'2px solid #5d5f5e',
+            borderRadius:'2px',
+            opacity:'0.7',  
+        })
+        addHoverEffect(cheackBoxDiv, [{
+            styleProp:'backgroundColor',
+            styleValue: '#eaebef',
+        }])
+        cheackBoxDiv.append(cheackBox)
+
+        const startIcondiv = createElement('div');
+        applyStyle(startIcondiv, {
+            width: '30px',
+            height: '30px',
+            borderRadius: '50%',
+            cursor: 'pointer',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            cursor:'pointor',
+        })
+        const startIcon = createImgContainer('emailContentStarIcon', './assests/main-section/starBaseline.png', 20, 20)
+
+        addHoverEffect(startIcondiv, [{
+            styleProp:'backgroundColor',
+            styleValue: '#eaebef',
+        }])
+        startIcondiv.append(startIcon)
         
-    })
-    cheackBoxListDropDownIcon.innerHTML = '<img style="opacity:0.7;" src="./assests/main-section/down.png" width="8" height="8" alt="image">'
+        // Sender name
+        const senderNameDiv = createElement('div');
+        senderNameDiv.id = item.id;
+        senderNameDiv.innerText = item.sender;
+        applyStyle(senderNameDiv, {
+            width:'100%',
+            maxWidth:'168px',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            whiteSpace: 'nowrap',
+            fontSize:'14px',
+            fontWeight: item.isRead ? 'normal' : 'bold',
+            color:item.isRead ? '#5F6368' : '#202124',
+        })
+        console.log(item)
 
-    addHoverEffect(cheackBoxListDropDownIcon, [{
-        styleProp:'backgroundColor',
-        styleValue: '#eaebef',
-    }])
 
-    cheackBoxListDiv.append(cheackBoxDiv, cheackBoxListDropDownIcon)
+        //Email content
+        const emailContentPreviewDiv = createElement('div');
+        emailContentPreviewDiv.id ='emailContentPreviewDiv';
+        emailContentPreviewDiv.innerHTML = `
+            <span id=emailSubject${i} 
+                style="
+                    color:${item.isRead ? '#5F6368' : '#202124'};
+                    font-weight:${item.isRead ? 'normal' : 'bold'}; 
+                    marginRight:5px;
+
+                ">
+                ${item.subject}
+            </span>
+            <span>
+                &nbsp;-&nbsp;
+            </span>
+            <span id=emailBody${i} 
+                style="
+                   color:#5F6368;
+                "
+            >
+                ${item.body}
+            </span>
+        `
+        applyStyle(emailContentPreviewDiv, {
+            width:'100%',
+            maxWidth:'468px',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            whiteSpace: 'nowrap',
+            fontSize:'14px',
+            fontWeight:'normal',
+        })
+        
+        //extra menu div
+        const emailContentExtraMenuDiv =  createElement('div');
+        emailContentExtraMenuDiv.id = 'emailContentExtraMenuDiv'+item.id;
+
+        applyStyle(emailContentExtraMenuDiv, {
+            width:'100%',
+            maxWidth:'fit-content',
+            marginLeft:'auto',
+            display:'flex',
+            alignItems:'center',
+            gap:'10px',
+            display:'none',
+        })
+
+        const linkButton = createElement('button');
+        linkButton.id = 'linkButton';
+        linkButton.innerText = 'Unsubscribe';
+        applyStyle(linkButton, {
+            fontSize:'12px',
+            backgroundColor:'#f2f6fc',
+            border:'1px solid #5F6368',
+            borderRadius:'3px',
+            padding:'3px 5px',
+        })
+        // addHoverEffect(linkButton,[{styleProp:'backgroundColor', styleValue:'#E8EAEC'}])
+
+        const archiveIconDiv = createElement('div');
+        archiveIconDiv.id = 'archiveIconDiv';
+        applyStyle(archiveIconDiv, {
+            width:'30px',
+            height:'30px',
+            borderRadius: '50%',
+            display:'flex',
+            justifyContent:'center',
+            alignItems:'center',
+        })
+        addHoverEffect(archiveIconDiv, [{styleProp:'backgroundColor', styleValue:'#eaebef'}])
+        const archiveIcon = createImgContainer('archiveIcon', './assests/main-section/archive.png', 20, 20)
+
+        archiveIconDiv.appendChild(archiveIcon) 
+
+        const deleteIconDiv = createElement('div');
+        deleteIconDiv.id = 'deleteIconDiv';
+        applyStyle(deleteIconDiv, {
+            width:'30px',
+            height:'30px',
+            borderRadius: '50%',
+            display:'flex',
+            justifyContent:'center',
+            alignItems:'center',
+        })
+        addHoverEffect(deleteIconDiv, [{styleProp:'backgroundColor', styleValue:'#eaebef'}])
+        const deleteIcon = createImgContainer('deleteIcon', './assests/main-section/delete.png', 20, 20)
+
+        deleteIconDiv.appendChild(deleteIcon) 
+
+        const draftIconDiv = createElement('div');
+        draftIconDiv.id = 'draftIconDiv';
+        applyStyle(draftIconDiv, {
+            width:'30px',
+            height:'30px',
+            borderRadius: '50%',
+            display:'flex',
+            justifyContent:'center',
+            alignItems:'center',
+        })
+        addHoverEffect(draftIconDiv, [{styleProp:'backgroundColor', styleValue:'#eaebef'}])
+        const draftIcon = createImgContainer('draftIcon', './assests/main-section/drafts.png', 20, 20)
+
+        draftIconDiv.appendChild(draftIcon) 
+
+        const emailClockIconDiv = createElement('div');
+        emailClockIconDiv.id = 'emailClockIconDiv';
+        applyStyle(emailClockIconDiv, {
+            width:'30px',
+            height:'30px',
+            borderRadius: '50%',
+            display:'flex',
+            justifyContent:'center',
+            alignItems:'center',
+        })
+        addHoverEffect(emailClockIconDiv, [{styleProp:'backgroundColor', styleValue:'#eaebef'}])
+        const emailClockIcon = createImgContainer('emailClockIcon', './assests/main-section/clockBaseline.png', 20, 20)
+
+        emailClockIconDiv.appendChild(emailClockIcon) 
+
+        emailContentExtraMenuDiv.append(linkButton, archiveIconDiv, deleteIconDiv, draftIconDiv,emailClockIconDiv)
+
+
+        //date container
+        const emailContentDate = createElement('div');
+        emailContentDate.id = 'emailContentDate'+item.id;
+        emailContentDate.innerText = item.date.split(',')[0]
+        applyStyle(emailContentDate, {
+            fontSize:'12px',
+            padding: '0px 10px',
+            color: item.isRead ? '#5F6368' : '#202124',
+            marginLeft:'auto',
+            fontWeight: item.isRead ? 'normal' : 'bold',
+
+        })
+
+
+        emailContentRow.append(cheackBoxDiv, startIcondiv, senderNameDiv, emailContentPreviewDiv,emailContentExtraMenuDiv, emailContentDate)
     
-    // referesh boc
-    const refereshIconDiv =  createElement('div');
-    refereshIconDiv.id = 'refereshIconDiv';
-    applyStyle(refereshIconDiv, {
-        width:'fit-content',
-        marginLeft:'10px',
-        padding:'10px',
-        borderRadius:'50%',
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        opacity:'0.7',
-    })
-
-    addHoverEffect(refereshIconDiv, [{
-        styleProp:'backgroundColor',
-        styleValue: '#eaebef',
-    }])
-
-    refereshIconDiv.innerHTML = `<img src="./assests/main-section/refresh.png" width="16" height="16" alt="image">`
+        //pop effect and visiblity of extramenu div
+        emailContentRow.addEventListener('mouseenter', function () {
+            emailContentRow.style.boxShadow = 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px';
+            emailContentRow.style.marginBottom = '1px';
+            emailContentPreviewDiv.style.maxWidth = '368px';
+            emailContentExtraMenuDiv.style.display = 'flex';
+            emailContentDate.style.display = 'none';
+        });
     
-    // referesh boc
-    const threeDotMenuIconDiv =  createElement('div');
-    threeDotMenuIconDiv.id = 'threeDotMenuIconDiv';
-    applyStyle(threeDotMenuIconDiv, {
-        width:'fit-content',
-        padding:'10px',
-        borderRadius:'50%',
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        opacity:'0.7',
+        emailContentRow.addEventListener('mouseleave', function () {
+            emailContentRow.style.boxShadow = '';
+            emailContentRow.style.marginBottom = '0px';
+            emailContentPreviewDiv.style.maxWidth = '468px';
+            emailContentExtraMenuDiv.style.display = 'none';
+            emailContentDate.style.display = 'block';
+        });
+        emailContenSubDiv.appendChild(emailContentRow)
     })
 
-    addHoverEffect(threeDotMenuIconDiv, [{
-        styleProp:'backgroundColor',
-        styleValue: '#eaebef',
-    }])
-
-    threeDotMenuIconDiv.innerHTML = `<img src="./assests/main-section/threeDots.png" width="16" height="16" alt="image">`
 
 
-    //pagination count section
-    const paginationCountDiv = createElement('div');
-    paginationCountDiv.id = 'paginationCountDiv';
-    applyStyle(paginationCountDiv, {
-        display:'flex',
-        alignItems:'center',
-        gap:'10px',
-    })
 
-    applyStyle(paginationCountDiv, {
-        width:'fit-content',
-        marginLeft: 'auto',
-    })
-
-    const paginationCountContent = createElement('div');
-    paginationCountContent.id = 'paginationCountContent';
-    applyStyle(paginationCountContent, {
-        fontSize:'12px',
-        color:'#5e5e5e',
-
-    })
-
-    paginationCountContent.innerHTML = `<span>1</span> - <span>50</span> of <span>70</span>`
+    emailContentDiv.append(emailContenSubDiv)
 
     
-    const navigationOfPaginationDiv = createElement('div');
-    navigationOfPaginationDiv.id = 'navigationOfPaginationDiv';
-    applyStyle(navigationOfPaginationDiv,{
-        display:'flex',
-        alignItems:'center',
-        justifyContent:'center',
-        gap:'20px'
-    })
-    
-    navigationOfPaginationDiv.innerHTML = `
-        <img style="transform:rotate(90deg)" src="./assests/main-section/more.png" width="16" height="16" alt="image">
-        <img style="transform:rotate(270deg)" src="./assests/main-section/more.png" width="16" height="16" alt="image">
-    `
-    paginationCountDiv.append(paginationCountContent, navigationOfPaginationDiv)
 
-
-    emailContentSectionHead.append(cheackBoxListDiv, refereshIconDiv, threeDotMenuIconDiv, paginationCountDiv)
-
-
-    const emailContentTable = createElement('table');
-    emailContentTable.id = 'emailContentTable';
-
-    const emailContentTehead = createElement('tbody');
-    emailContentTehead.id = 'emailContentTehead';
-
-
-
-
-    emailContentSection.append(emailContentSectionHead, emailContentTable)
+    emailContentSection.append(EmailContentSectionHeader(), emailContentDiv)
 
     return emailContentSection
 }
